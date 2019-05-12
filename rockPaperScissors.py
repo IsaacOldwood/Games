e979febbc05e6381076920d4230862aec5eafd2c
Isaac Oldwood <isaac.oldwood@gmail.com>
2019-05-12 18:17:29 +0100

    Initialise

---
+++ rockPaperScissors.py
@@ -0,0 +1,37 @@
+import random
+
+def main():
+    print(aiChoose())
+
+def playerChoose():
+    return input('Choose Your Weapon: ')
+
+def aiChoose():
+    return random.choice(['Rock','Paper','Scissors'])
+
+def checkWinner(playerChoice,aiChoice):
+    if playerChoice == 'Rock':
+        if aiChoice == 'Rock':
+            return 'Draw'
+        elif aiChoice == 'Paper':
+            return 'Lose'
+        elif aiChoice == 'Scissors':
+            return 'Win'
+    elif playerChoice == 'Paper':
+        if aiChoice == 'Rock':
+            return 'Win'
+        elif aiChoice == 'Paper':
+            return 'Draw'
+        elif aiChoice == 'Scissors':
+            return 'Lose'
+    elif playerChoice == 'Scissors':
+        if aiChoice == 'Rock':
+            return 'Lose'
+        elif aiChoice == 'Paper':
+            return 'Win'
+        elif aiChoice == 'Scissors':
+            return 'Draw'
+
+
+if __name__=="__main__":
+    main()
